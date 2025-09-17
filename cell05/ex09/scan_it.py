d25c83c38d689c7c9914078dc03db3051d51143f
aomxnachh <specialmmr@gmail.com>
2025-09-17 14:28:00 +0700

    com5

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+if len(sys.argv) != 3:
+    print("none")
+else:
+    keyword = sys.argv[1]
+    string = sys.argv[2]
+    occurrences = re.findall(rf"{re.escape(keyword)}", string)
+    if occurrences:
+        print(len(occurrences))
+    else:
+        print("none")
